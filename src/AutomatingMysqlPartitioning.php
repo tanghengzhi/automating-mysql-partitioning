02c89eb769da3cae337c4a9ee7646e59375b9945
Victor Tang <tanghengzhi@live.com>
2025-01-22 17:12:38 +0800

    add & drop

--- src/AutomatingMysqlPartitioning.php
+++ src/AutomatingMysqlPartitioning.php
@@ -2,4 +2,20 @@
 
 namespace VictorTang\AutomatingMysqlPartitioning;
 
-class AutomatingMysqlPartitioning {}
+class AutomatingMysqlPartitioning {
+
+    public function __construct(public string $table)
+    {
+
+    }
+
+    public function addPartition()
+    {
+        \DB::statement("ALTER TABLE {$this->table} ADD PARTITION (PARTITION p202501 VALUES LESS THAN (2025-02-01 00:00:00);");
+    }
+
+    public function dropPartition()
+    {
+        \DB::statement("ALTER TABLE {$this->table} DROP PARTITION p202501;");
+    }
+}
